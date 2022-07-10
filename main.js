2d0792084064ab5bbd7cb257bc3f99c2a4a614d8
Matthew Zenn <spyguy57@gmail.com>
2022-07-10 18:23:03 -0400

    Images now transfer canvases at lower bit-depth

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const fakeButton = document.getElementById("open");
 const image = document.getElementById("image");
 const canvas1 = document.getElementById("canvas1");
 const ctx = canvas1.getContext("2d");
+const canvas2 = document.getElementById("canvas2");
+const ctx1 = canvas2.getContext("2d");
 
 /**
  * Maps the button for selecting an image.
@@ -28,4 +30,24 @@ realButton.addEventListener("change", function() {
     }
 });
 
-ctx.drawImage(image, 0, 0);
+image.addEventListener('load', e => {
+    ctx.clearRect(0, 0, canvas1.width, canvas1.height);
+    ctx.drawImage(image, 0, 0, 200, 200);
+    var imgdata = ctx.getImageData(0, 0, 200, 200); // get data
+    var data = imgdata.data; // bytes
+
+      // 4-bit: rrr ggg bb
+    for(var i = 0; i < data.length; i += 4) {
+      data[i]     = nearest(data[i],     4); // set value to nearest of 4 possibilities
+      data[i + 1] = nearest(data[i + 1], 4);
+      data[i + 2] = nearest(data[i + 2], 2);
+    }
+
+    ctx1.putImageData(imgdata, 0, 0); // put image data to canvas
+
+    function nearest(x, a) { // will round down to nearest of a possibilities
+                         // in the range 0 <= x <= 255
+        return Math.floor(x / (255 / a)) * (255 / a);
+    }
+    console.log(ctx.getImageData(0, 0, 200, 200));
+});
