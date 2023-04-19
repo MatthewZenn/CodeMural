7efa8eaa459b024811c53d4388da52e0240b3e57
Matthew Zenn <spyguy57@gmail.com>
2023-04-19 17:34:06 -0400

    Tweaked UI

--- logic.js
+++ logic.js
@@ -1,7 +1,22 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext('2d');
 
+var x = 0;
+
 document.getElementById("run").addEventListener('click', function() {
-    ctx.clearRect(0, 0, canvas.height, canvas.width);
+    ctx.clearRect(0, 0, canvas.height*2, canvas.width*2);
     eval(document.getElementById("editor").value);
 })
+
+document.getElementById("edit").addEventListener('click', function() {
+    if (x == 0) {
+        document.getElementById("editor").style.display = "none";
+        document.getElementById("edit").innerHTML = "&#xE70F;"
+        x = 1;
+    }
+    else {
+        document.getElementById("editor").style.display = "block";
+        document.getElementById("edit").innerHTML = "&#xE890;"
+        x = 0;
+    }
+});
