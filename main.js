24effcf657f20f49d91ee65e45e474ca71587cfe
Matthew Zenn <spyguy57@gmail.com>
2023-04-18 21:16:52 -0400

    fixing resize issue

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const {app, BrowserWindow, Menu} = electron;
 let mainWindow;
 
 app.on('ready', function(){
-    mainWindow = new BrowserWindow({width: 1920, height: 1080, resizable: false, frame:false, icon: "Assets/Animate.ico"});
+    mainWindow = new BrowserWindow({width: 1920, height: 1080, resizable: true, frame:false, icon: "Assets/Animate.ico"});
     mainWindow.loadURL(url.format({
         pathname: path.join(__dirname, 'mainWindow.html'),
         protocol: 'file:',
